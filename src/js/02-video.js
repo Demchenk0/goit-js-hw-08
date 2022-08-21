6c3db348717a0be6bc44c9ae144ba66d50c1e77e
Demchenk0 <santadyr2016@gmail.com>
2022-08-21 21:49:44 +0300

    2завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+console.log(throttle(playTime, 1000));
+
+
+    function playTime(evt) {
+        localStorage.setItem('videoplayer-current-time', evt.seconds);
+};
+const numberKey = Number(localStorage.getItem('videoplayer-current-time'));
+
+        if (numberKey) {
+            player.setCurrentTime(numberKey);
+        };
+console.log(numberKey);
